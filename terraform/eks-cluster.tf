45ce0a03837c91a2f7b815901fefb8894b8b092e
wbrymo <wbrymo@gmail.com>
2025-06-13 04:13:32 +0100

    new

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -7,12 +7,11 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  eks_managed_node_groups = {
-    default = {
-      desired_size   = 2
-      max_size       = 3
-      min_size       = 1
-      instance_types = ["t3.micro"]
-    }
+eks_managed_node_groups = {
+  default = {
+    desired_size   = 1
+    max_size       = 1
+    min_size       = 1
+    instance_types = ["t3.nano"]  # Uses 1 vCPU
   }
 }
